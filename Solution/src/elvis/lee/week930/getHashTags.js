9060f59a014867c4bd97579fc991c8538fbd37c0
elvis-ican <62309426+elvis-ican@users.noreply.github.com>
2021-09-20 18:31:08 -0600

    submit solution

--- Solution/src/elvis/lee/week930/getHashTags.js
+++ Solution/src/elvis/lee/week930/getHashTags.js
@@ -1,5 +1,5 @@
 function getHashTags(s) {
-    let words = s.split(' ');
+    let words = s.replaceAll(',', '').split(' ');
     words.sort((a, b) => b.length - a.length);
     var tags;
     if (words.length < 3) {
