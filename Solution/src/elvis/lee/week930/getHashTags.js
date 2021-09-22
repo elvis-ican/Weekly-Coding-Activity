95be02301ac93574921ddf7beeb8f87047b9a659
elvis-ican <62309426+elvis-ican@users.noreply.github.com>
2021-09-22 10:00:29 -0600

    add console.log

--- Solution/src/elvis/lee/week930/getHashTags.js
+++ Solution/src/elvis/lee/week930/getHashTags.js
@@ -1,14 +1,13 @@
 function getHashTags(s) {
     let words = s.replaceAll(',', '').split(' ');
     words.sort((a, b) => b.length - a.length);
-    var tags;
-    if (words.length < 3) {
-        tags = words;
-    } else {
-        tags = words.slice(0, 3);
-    }
-    for (let t of tags) {
-        t = '#' + t.toLowerCase();
+    let tags = [];
+    for (let i = 0; i < 3 && i < words.length; i++) {
+        tags.push('#' + words[i].toLowerCase());
     }
     return tags;
 }
+
+console.log(getHashTags("Why You Will Probably Pay More for Your Christmas Tree This Year"));
+console.log(getHashTags("Hey Parents, Surprise, Fruit Juice Is Not Fruit"));
+console.log(getHashTags("Visualizing Science"));
